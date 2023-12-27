d50a22f790a75c34b5f19cf68b7a4fe5b154e05b
NfoTECH <fortunenwachukwu@gmail.com>
2023-12-27 07:47:40 +0100

    Add the parent pom.xml file

---
+++ src/main/java/com/fortunate/kafkarealwikimedia/KafkaRealWikimediaApplication.java
@@ -0,0 +1,13 @@
+package com.fortunate.kafkarealwikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealWikimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealWikimediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortunate/kafkarealwikimedia/KafkaRealWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortunate.kafkarealwikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
